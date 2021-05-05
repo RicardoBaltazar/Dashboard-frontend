54a9808938707929a382a1631807ae6c8f7847c2
Ricardo Baltazar <ricardo.baltazar04@gmail.com>
2021-05-05 20:53:07 -0300

    parte 1

---
+++ redux-app/src/App.jsx
@@ -0,0 +1,21 @@
+import styled from './styled.module.scss';
+
+function App() {
+  return (
+    <>
+    <div className={styled.app}>
+
+      <div className={styled.card}>
+        <input type="number" name="" id="" />
+      </div>
+
+      <div className={styled.card}>
+        <input type="number" name="" id="" />
+      </div>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
