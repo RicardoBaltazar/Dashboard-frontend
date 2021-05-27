1c0b0033329f09865a20402d5b3d3059a6f091f8
Ricardo Baltazar <ricardo.baltazar04@gmail.com>
2021-05-26 22:46:28 -0300

    criando formulário e enviando dados por submit

--- app/src/screen/register/index.tsx
+++ app/src/screen/register/index.tsx
@@ -1,40 +1,61 @@
+import React, { useState } from 'react';
 import styled from './styled.module.scss';
 
-const pageRegister = () => {
+
+
+const PageRegister = () => {
+
+    const [ name, setName ] = useState('');
+    const [ cnpj, setCnpj ] = useState('');
+    const [ city, setCity ] = useState('');
+    const [ cep, setCep ] = useState('');
+    const [ openDate, setOpenDate ] = useState('');
+
+    const handleSubmit = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        console.log(
+            name,
+            cnpj,
+            city,
+            cep,
+            openDate,
+        )
+    }
+
     return (
         <>
-            <section className={styled.section}>
+            <form className={styled.form} onSubmit={handleSubmit}>
                 <div className={styled.card}>
                     <div className={styled.headerCard}>
                         <p>Empresas/CADASTRAR EMPRESAS</p>
-                        <button>SALVAR</button>
+                        <button type='submit'>SALVAR</button>
                     </div>
-                    <form className={styled.form}>
+                    <div className={styled.section}>
                         <div>
                             <label htmlFor="">Nome Da Empresa</label>
-                            <input type="text" />
+                            <input type="text" onChange={e => setName(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="">Cnpj</label>
-                            <input type="number" />
+                            <input type="text" onChange={e => setCnpj(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="">Cidade/UF</label>
-                            <input type="text" />
+                            <input type="text" onChange={e => setCity(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="">CEP</label>
-                            <input type="number" />
+                            <input type="text" onChange={e => setCep(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="">Data de Abertura</label>
-                            <input type="date" />
+                            <input type="text" onChange={e => setOpenDate(e.target.value)}/>
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </section>
+            </form>
         </>
     )
 }
 
-export default pageRegister;
+export default PageRegister;
