1df915df4a5b96d645c3f58ba6807c2b1401f421
Ricardo Baltazar <ricardo.baltazar04@gmail.com>
2021-05-26 21:52:42 -0300

    configurando visual do formulário para registrar empresa

--- app/src/screen/register/index.tsx
+++ app/src/screen/register/index.tsx
@@ -1,8 +1,38 @@
+import styled from './styled.module.scss';
 
 const pageRegister = () => {
     return (
         <>
-            <h1>Tela cadastro</h1>
+            <section className={styled.section}>
+                <div className={styled.card}>
+                    <div className={styled.headerCard}>
+                        <p>Empresas/CADASTRAR EMPRESAS</p>
+                        <button>SALVAR</button>
+                    </div>
+                    <form className={styled.form}>
+                        <div>
+                            <label htmlFor="">Nome Da Empresa</label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label htmlFor="">Cnpj</label>
+                            <input type="number" />
+                        </div>
+                        <div>
+                            <label htmlFor="">Cidade/UF</label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label htmlFor="">CEP</label>
+                            <input type="number" />
+                        </div>
+                        <div>
+                            <label htmlFor="">Data de Abertura</label>
+                            <input type="date" />
+                        </div>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
